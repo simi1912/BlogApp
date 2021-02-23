845c0190742ead099b85ca608af195b7b27d55cc
EC2 Default User <ec2-user@ip-172-31-25-217.us-east-2.compute.internal>
2021-02-23 13:03:18 +0000

    Added PUT endpoint

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express     = require("express"),
+    methodOverride = require("method-override"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
     app         = express();
@@ -8,6 +9,7 @@ mongoose.connect("mongodb://localhost:27017/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 
 
@@ -20,11 +22,6 @@ var blogSchema = new mongoose.Schema({
 
 var Blog = mongoose.model("Blog", blogSchema);
 
-// Blog.create({
-//     tiltle: "Test Blog",
-//     image: "img.jpg",
-//     body: "Brlog post"
-// });
 
 
 app.get("/", function(req, res) {
@@ -46,14 +43,13 @@ app.get("/blogs/new", function(req, res){
 });
 
 app.post("/blogs", function(req, res){
-    
-   Blog.create(req.body.blog, function(err, newBlog){
-       if(err){
-           res.render("new");
-       } else{
-           res.redirect("/blogs");
-       }
-   });
+  Blog.create(req.body.blog, function(err, newBlog){
+      if(err){
+          res.render("new");
+      } else{
+          res.redirect("/blogs");
+      }
+  });
 });
 
 app.get("/blogs/:id", function(req, res) {
@@ -65,6 +61,31 @@ app.get("/blogs/:id", function(req, res) {
         }
     });
 });
+
+app.get("/blogs/:id/edit", function(req, res) {
+     Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+})
+
+app.put("/blogs/:id", function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/"+req.params.id);
+        }
+    });
+});
+
+app.delete("/blogs/:id", function(req, res){
+  res.send("Delete") ;
+});
+
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
